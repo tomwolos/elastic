54960d477f64d37cfa606e4c3d0e1a24adacc8f2
Tomek Woloszynski <tomekwoloszynski@gmail.com>
2020-01-20 13:40:46 +0800

    minor updates

--- elastic.py
+++ elastic.py
@@ -3,57 +3,58 @@ import pyfftw as pf
 
 
 class Elastic:
-    """Elastic deformation for a given normal stress.
+    """Elastic deformation due to normal stress.
 
-    Perform FFTW planning and calculate the elastic deformation for a given
-    normal stress distribution. Requires numpy and pyfftw modules.
+    Perform FFTW planning and calculate the elastic deformation due to given
+    normal stress distribution using half-space approximation. The influence
+    coefficients and their FFT are saved in coefficients{nx}x{ny}.npz (see
+    below for the description of nx and ny).
+    Requires numpy and pyfftw.
 
     Parameters
     ----------
     nx, ny : int, int
-        numbers of points
+        numbers of points used for the discretisation of stress/deformation
     scale : float
-        scaling factor for non-unit domain size (default 1.0)
+        scaling factor for domains other than the unit square (default 1.0)
     modulus : float
         elastic modulus (default 1.0)
-    ncoeffs : int
-        number of influence coefficients to store, including the centre
-        (default 3); these coefficients are typically used in derivative-based
-        methods, for example ncoeffs=2 for first-order upwind and ncoeffs=3 for
-        second-order upwind
+    path : string
+        path to the coefficients file (default ""), including the trailing /
+        set path=None if you do not want to save the coefficients
     verbose : bool
-        print when FFT of influence coefficients is being calculated
+        print when generating influence coefficients (default True)
 
     Example
     -----
     from elastic import Elastic
     import numpy as np
 
-    elastic = Elastic(128, 128, modulus=2.0)
-    pressure = np.random.rand(128, 128)
-    deformation = elastic.deformation(pressure)
+    N = 128
+    elastic = Elastic(N, N, path="/home/user/coefficients/")
+    stress = np.random.rand(N, N)
+    deformation = elastic.deformation(stress)
 
-    print(elastic.coefficients[0])
+    print(elastic.coefficients[-1][-3:])
     """
 
     def __init__(self, nx: int,
                  ny: int,
                  scale: float=1.0,
                  modulus: float=1.0,
-                 ncoeffs: int=3,
+                 path: str="",
                  verbose: bool=True):
         self.nx = nx
         self.ny = ny
         self.scale = scale
         self.modulus = modulus
-        self.ncoeffs = ncoeffs
+        self.path = path
         self.verbose = verbose
 
         self.x = np.linspace(0, 1, nx)
         self.y = np.linspace(0, ny/nx, ny)
         self.dx = self.x[1] - self.x[0]
         self.dy = self.y[1] - self.y[0]
-        self.coefficients = np.zeros((self.ncoeffs,))
 
         # FFTW planning
         self.fft_in = pf.empty_aligned((2*nx, 2*ny), 'float64')
@@ -64,23 +65,27 @@ class Elastic:
         self.ifft = pf.FFTW(self.ifft_in, self.ifft_out, axes=(0, 1),
                             direction='FFTW_BACKWARD')
 
-        # load or generate and save FFT of the influence coefficients
+        # load/generate and save the influence coefficients and their FFT
         self.fftcoefficients = pf.empty_aligned((2*nx, ny+1), 'complex128')
-        filename = f"coefficients{nx}x{ny}.npz"
-        try:
-            temp = np.load(filename)
-            self.fftcoefficients[:] = temp['fftcoefficients'][:]
-            self.coefficients[:] = temp['coefficients'][:]
-        except IOError:
+        self.coefficients = np.zeros((nx, ny))
+        if path is not None:
+            filename = f"{path}coefficients{nx}x{ny}.npz"
+            try:
+                temp = np.load(filename)
+                self.fftcoefficients[:] = temp['fftcoefficients'][:]
+                self.coefficients[:] = temp['coefficients'][:]
+            except IOError:
+                self.__generate()
+                np.savez(filename, fftcoefficients=self.fftcoefficients,
+                         coefficients=self.coefficients)
+        else:
             self.__generate()
-            np.savez(filename, fftcoefficients=self.fftcoefficients,
-                     coefficients=self.coefficients)
         self.fftcoefficients *= self.scale/np.pi/self.modulus
         self.coefficients *= self.scale/np.pi/self.modulus
 
     def __generate(self):
         if self.verbose:
-            print("Generating FFT of the influence coefficients", flush=True)
+            print("Generating influence coefficients", flush=True)
         for jx in range(self.nx):
             for jy in range(self.ny):
                 x1 = self.x[jx] - self.x[-1] + self.dx/2
@@ -95,8 +100,7 @@ class Elastic:
                 self.fft_in[-2-jx][jy] = self.fft_in[jx][jy]
                 self.fft_in[jx][-2-jy] = self.fft_in[jx][jy]
                 self.fft_in[-2-jx][-2-jy] = self.fft_in[jx][jy]
-        self.coefficients[:] = (
-            self.fft_in[self.nx][self.ny:self.ny+self.ncoeffs])
+        self.coefficients[:, :] = self.fft_in[:self.nx, :self.ny]
         self.fft.execute()
         self.fft_in[:] = 0
         self.fftcoefficients[:] = self.fft_out[:]
---
+++ examples.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from elastic import Elastic
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import numpy as np
+
+Nx = 2*2*2*128
+Ny = 2*2*2*128
+L = 6
+
+tx = np.linspace(-L/2, L/2, Nx)
+ty = np.linspace(-L/2, L/2, Ny)
+(x, y) = np.meshgrid(tx, ty)
+
+pressure = np.random.rand(Nx, Ny)
+elastic = Elastic(Nx, Ny, path="/home/tom/work/coefficients/")
+deformation = elastic.deformation(pressure)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_wireframe(x, y, deformation)
+
+plt.show(block=True)
