correct domain scaling

elastic.py
@@ -3,73 +3,70 @@ import pyfftw as pf
 
 
 class Elastic:
-    """Elastic deformation due to normal stress.
+    """Deformation of an elastic half-space for a given normal stress
 
+    Calculate the deformation of an elastic half-space for a given normal
+    stress distribution using FFT. This is done by numerically calculating
+    the integral of
+        p(x', y')/sqrt((x - x')**2 + (y - y')**2)dx'dy'
+    over the 
+    Boussinesq problem for a given normal stress distribution. due to a given normal
+    stress distribution by numerically solving the Boussinesq problem using
+    the FFT. stress distribution of an
+    elastic half-space using Boussinesq 
+    of an 
     Perform FFTW planning and calculate the elastic deformation due to given
     normal stress distribution using half-space approximation. The influence
-    coefficients and their FFT are saved in coefficients{nx}x{ny}.npz (see
+    coefficients and their FFT are stored in coefficients{nx}x{ny}.npz (see
     below for the description of nx and ny).
-    Requires numpy and pyfftw.
+    Dependencies: numpy, pyfftw
 
-    Parameters
+    Attributes
     ----------
     nx, ny : int, int
         numbers of points used for the discretisation of stress/deformation
     scale : float
-        scaling factor for domains other than the unit square (default 1.0)
-    modulus : float
-        elastic modulus (default 1.0)
+        scaling factor for the x direction (default 1.0);
+        the y direction is scaled by ny/nx*scale
     path : string
-        path to the coefficients file (default ""), including the trailing /
+        path to the coefficients file (default '');
         set path=None if you do not want to save the coefficients
     verbose : bool
         print when generating influence coefficients (default True)
 
-    Example
-    -----
-    from elastic import Elastic
-    import numpy as np
-
-    N = 128
-    elastic = Elastic(N, N, path="/home/user/coefficients/")
-    stress = np.random.rand(N, N)
-    deformation = elastic.deformation(stress)
-
-    print(elastic.coefficients[-1][-3:])
+    Methods
+    ------
+    update(stress : numpy.ndarray) -> numpy.ndarray
+        calculate and return the deformation for the given normal stress
     """
 
     def __init__(self, nx: int,
                  ny: int,
-                 scale: float=1.0,
-                 modulus: float=1.0,
-                 path: str="",
+                 scale: 1.0,
+                 path: str='',
                  verbose: bool=True):
         self.nx = nx
         self.ny = ny
         self.scale = scale
-        self.modulus = modulus
         self.path = path
         self.verbose = verbose
-
         self.x = np.linspace(0, 1, nx)
         self.y = np.linspace(0, ny/nx, ny)
         self.dx = self.x[1] - self.x[0]
         self.dy = self.y[1] - self.y[0]
 
         # FFTW planning
-        self.fft_in = pf.empty_aligned((2*nx, 2*ny), 'float64')
-        self.fft_out = pf.empty_aligned((2*nx, ny+1), 'complex128')
-        self.ifft_in = pf.empty_aligned((2*nx, ny+1), 'complex128')
-        self.ifft_out = pf.empty_aligned((2*nx, 2*ny), 'float64')
-        self.fft = pf.FFTW(self.fft_in, self.fft_out, axes=(0, 1))
-        self.ifft = pf.FFTW(self.ifft_in, self.ifft_out, axes=(0, 1),
+        self.real = pf.empty_aligned((2*nx, 2*ny), 'float64')
+        self.complex = pf.empty_aligned((2*nx, ny+1), 'complex128')
+        self.fft = pf.FFTW(self.real, self.complex, axes=(0, 1))
+        self.ifft = pf.FFTW(self.complex, self.real, axes=(0, 1),
                             direction='FFTW_BACKWARD')
 
-        # load/generate and save the influence coefficients and their FFT
+        # load or generate and save the influence coefficients and their FFT
         self.fftcoefficients = pf.empty_aligned((2*nx, ny+1), 'complex128')
         self.coefficients = np.zeros((nx, ny))
         if path is not None:
-            filename = f"{path}coefficients{nx}x{ny}.npz"
+            filename = f'{path}coefficients{nx}x{ny}.npz'
             try:
                 temp = np.load(filename)
                 self.fftcoefficients[:] = temp['fftcoefficients'][:]
@@ -80,36 +77,39 @@ class Elastic:
                          coefficients=self.coefficients)
         else:
             self.__generate()
-        self.fftcoefficients *= self.scale/np.pi/self.modulus
-        self.coefficients *= self.scale/np.pi/self.modulus
+        self.fftcoefficients *= self.scale
+        self.coefficients *= self.scale
 
     def __generate(self):
         if self.verbose:
-            print("Generating influence coefficients", flush=True)
+            print(f'Generating influence coefficients '
+                  f'({self.nx} x {self.ny})', flush=True)
         for jx in range(self.nx):
             for jy in range(self.ny):
                 x1 = self.x[jx] - self.x[-1] + self.dx/2
                 x2 = self.x[jx] - self.x[-1] - self.dx/2
                 y1 = self.y[jy] - self.y[-1] + self.dy/2
                 y2 = self.y[jy] - self.y[-1] - self.dy/2
-                self.fft_in[jx][jy] = (
+                self.real[jx][jy] = (
                     np.abs(x1)*(np.arcsinh(y1/x1) - np.arcsinh(y2/x1))
                     + np.abs(x2)*(np.arcsinh(y2/x2) - np.arcsinh(y1/x2))
                     + np.abs(y1)*(np.arcsinh(x1/y1) - np.arcsinh(x2/y1))
                     + np.abs(y2)*(np.arcsinh(x2/y2) - np.arcsinh(x1/y2)))
-                self.fft_in[-2-jx][jy] = self.fft_in[jx][jy]
-                self.fft_in[jx][-2-jy] = self.fft_in[jx][jy]
-                self.fft_in[-2-jx][-2-jy] = self.fft_in[jx][jy]
-        self.coefficients[:, :] = self.fft_in[:self.nx, :self.ny]
+                self.real[-2-jx][jy] = self.real[jx][jy]
+                self.real[jx][-2-jy] = self.real[jx][jy]
+                self.real[-2-jx][-2-jy] = self.real[jx][jy]
+        self.coefficients[:] = self.real[:self.nx, :self.ny]
         self.fft.execute()
-        self.fft_in[:] = 0
-        self.fftcoefficients[:] = self.fft_out[:]
+        self.real[:] = 0.0
+        self.fftcoefficients[:] = self.complex
         if self.verbose:
-            print("done.\n", flush=True)
+            print('done.\n', flush=True)
 
-    def deformation(self, pressure):
-        self.fft_in[:self.nx, :self.ny] = pressure[:]
+    def update(self, stress: np.ndarray) -> np.ndarray:
+        self.real[:] = 0.0
+        self.real[:self.nx, :self.ny] = stress 
         self.fft.execute()
-        self.ifft_in[:] = self.fftcoefficients[:]*self.fft_out[:]
+        self.complex[:] *= self.fftcoefficients
         self.ifft.execute()
-        return self.ifft_out[-1-self.nx:-1, -1-self.ny:-1]/(4*self.nx*self.ny)
+        self.real[-1-self.nx:-1, -1-self.ny:-1] /= 2*self.nx*2*self.ny
+        return self.real[-1-self.nx:-1, -1-self.ny:-1]

examples.py
@@ -6,20 +6,43 @@ from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 import numpy as np
 
-Nx = 128
-Ny = 128
-L = 6
+Nx = 256
+Ny = 256
+L = 4
+scale = L
+path = '/home/tom/work/coefficients/'
 
 tx = np.linspace(-L/2, L/2, Nx)
 ty = np.linspace(-L/2, L/2, Ny)
-(x, y) = np.meshgrid(tx, ty)
+(x, y) = np.meshgrid(tx, ty, indexing='ij')
 
-pressure = np.random.rand(Nx, Ny)
-elastic = Elastic(Nx, Ny, path="/home/tom/work/coefficients/")
-deformation = elastic.deformation(pressure)
+# Hertzian contact
+stress = np.zeros((Nx, Ny))
+z = x**2 + y**2
+stress[z < 1] = np.sqrt(1 - z[z < 1])
+elastic = Elastic(Nx, Ny, scale, path)
+deformation = 2/(np.pi**2)*elastic.update(stress)
+gap = x**2/2 + y**2/2 + deformation
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Stress')
+ax.plot_wireframe(x, y, stress)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Elastic deformation')
 ax.plot_wireframe(x, y, deformation)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Gap')
+ax.plot_wireframe(x, y, gap)
 
 plt.show(block=True)